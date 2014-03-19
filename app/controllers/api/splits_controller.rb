65081617dd707de0cc7d55c171486ffb3e68fc66
Charlie <cleathers89@gmail.com>
2014-03-19 14:54:40 -0700

    split model able to submit through console

--- app/controllers/api/splits_controller.rb
+++ app/controllers/api/splits_controller.rb
@@ -3,38 +3,42 @@ class Api::SplitsController < ApplicationController
   def create
     # creates new split object
     new_split = Split.new()
-    new_split.total_amt = split_params.amt
-    new_split.des = split_params.des
+
+    p split_params
+    p friend_params
+
+    new_split.total_amt = split_params['amt']
+    new_split.des = split_params['des']
 
     # creates usersplit for current User
     current_user_split = UserSplit.new()
 
-    current_user_split.amt = split_params.user_amt
-    current_user_split.split_id = new_split.id
-    current_user_split.split_type = split_params.split_type
+    current_user_split.amt = split_params['user_amt']
+    current_user_split.split_id = new_split['id']
+    current_user_split.split_type = split_params['split_type']
     current_user_split.user_id = current_user.id
 
-    friend_split_type = (split_params.split_type == 'positive') ? 'negative' : 'positive'
+    friend_split_type = (split_params['split_type'] == 'positive') ? 'negative' : 'positive'
 
     begin
       ActiveRecord::Base.transaction do
         # creates Split
-        new_split.create!
+        new_split.save!
         
         # creates User_split for each friend and many for current_user
-        split_params.friends.each do |friend|
+        friend_params.each do |friend|
           friend_split = UserSplit.new()
           
-          friend_split.amt = friend.amt
+          friend_split.amt = friend['amt']
           friend_split.friend_id = current_user.id
-          friend_split.split_id = new_split.id
+          friend_split.split_id = new_split['id']
           friend_split.split_type = friend_split_type
-          friend_split.user_id = friend.id
+          friend_split.user_id = friend['id']
 
-          current_user_split.friend_id = friend.id
+          current_user_split.friend_id = friend['id']
           
-          friend_split.create!
-          current_user_split.create!
+          friend_split.save!
+          current_user_split.save!
         end
       end # end transaction
     
@@ -60,6 +64,10 @@ class Api::SplitsController < ApplicationController
 
   private
     def split_params
-      params.require(:split).permit(:des, :friends, :user_amt, :amt, :split_type)
+      params.permit(:des, :user_amt, :amt, :split_type)
+    end
+
+    def friend_params
+      params.permit(:friends => [:amt, :id]).require(:friends)
     end
 end
