a48541354d43132fc47e12f256ca1ce3af8ad2b3
Charlie <cleathers89@gmail.com>
2014-03-18 19:42:46 -0700

    initial commit, added bootstrap, removed turbolinks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BillPinClone::Application.config.secret_key_base = 'f808ec9eb7a9436a4aa3db1f1742c3be60709ceb7c3597ce2db0d7e5bbb94b27f6ad3eed0792401e0f629ddf6acf483454f3bf0a57d40bfdb52c96b70a3850aa'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+BillPinClone::Application.routes.draw do
+  root to: 'landing#index'
+end
