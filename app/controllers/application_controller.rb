6c3130e76052539714073797a4f896bead3cae8b
Charlie <cleathers89@gmail.com>
2014-03-18 23:18:21 -0700

    added guest user route, applicationController helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,35 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  
+  helper_method :current_or_guest_user
 
+  def current_or_guest_user
+    if current_user
+      if session[:guest_user_id]
+        guest_user.destroy
+        session[:guest_user_id] = nil
+      end
+      current_user
+    else
+      guest_user
+      redirect_to root_url
+    end
+  end
+
+  def guest_user
+    @cached_guest_user ||= User.find(session[:guest_user_id] ||= create_guest_user.id)
+
+    rescue ActiveRecord::RecordNotFound # if session[:guest_user_id] invalid
+      session[:guest_user_id] = nil
+      guest_user
+  end
+
+  private
+
+  def create_guest_user
+    u = User.create(email: "guest_#{Time.now.to_i}#{rand(99)}@example.com")
+    u.save!(validate: false)
+    session[:guest_user_id] = u.id
+    u
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 BillPinClone::Application.routes.draw do
   root to: 'home#index'
   devise_for :users
+  post '/users/guest', to: 'application#current_or_guest_user'
 end
