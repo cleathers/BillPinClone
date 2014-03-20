205797b09608c49010637cf8068ccec1f58e436f
Charlie <cleathers89@gmail.com>
2014-03-19 21:43:24 -0700

    Added Composite View Support on Backbone

--- app/assets/javascripts/bill_pin_clone.js
+++ app/assets/javascripts/bill_pin_clone.js
@@ -15,3 +15,60 @@ window.BillPinClone = {
     });
   }
 };
+
+Backbone.CompositeView = Backbone.View.extend({
+
+  addSubview: function (selector, subview) {
+    var selectorSubviews =
+      this.subviews()[selector] || (this.subviews()[selector] = []);
+
+    selectorSubviews.push(subview);
+    
+    var $selectorEl = this.$(selector);
+    $selectorEl.append(subview.$el);
+  },
+
+  remove: function () {
+    // calls original remove on the parent view
+    Backbone.View.prototype.remove.call(this);
+
+    // then removes all subviews from the view
+    _(this.subviews()).each(function (selectorSubviews, selector) {
+      _(selectorSubviews).each(funciton (subview) {
+        subview.remove();
+      });
+    });
+  },
+
+  removeSubview: function (selector, subview) {
+    var selectorSubviews =
+      this.subviews()[selector] || (this.subviews()[selector] = []);
+
+    var subviewIndex = selectorSubviews.indexOf(subview);
+    selectorSubviews.splice(subviewIndex, 1);
+    subview.remove();
+  }
+
+  renderSubviews: function () {
+    var view = this;
+
+    _(this.subviews()).each(function (selectorSubviews, selector) {
+      var $selectorEl = view.$(selector);
+      $selectorEl.empty();
+
+      _(selectorSubviews).each(function (subview) {
+        $selectorEl.append(subview.render().$el);
+        subview.delegateEvents();
+      });
+    });
+  },
+
+  subviews: function () {
+    if (!this._subviews) {
+      this._subviews = {};
+    }
+    
+    return this._subviews;
+  }
+
+});
